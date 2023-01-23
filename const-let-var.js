d22ba01176662512ca05e9c48b067f18b5c96301
Joel Moreira <joelmoreira.dev@gmail.com>
2023-01-23 07:05:29 -0300

    Subida final de const-let-var

--- const-let-var.js
+++ const-let-var.js
@@ -6,11 +6,18 @@
 // Este let nombre no es el mismo que el que está dentro del if.
 let nombre = 'Wolverine';
 
+// ? las constantes funcionan como let (funciona dentro de un mismo scope), pero su valor nunca puede cambiarse.
+// const nombre = 'Wolverine';
+
 if(true) {
     // este let es diferente al que está fuera del if.
+    // cuando se re-declara una variable, se usa esta.
+    // si en lugar de declararla, solo le asignara un nuevo valor a nombre, node buscaria el let del scope superior (arriba del if)
     let nombre = 'Magneto';
     console.log(nombre); // imprime Magneto
 }
 
 
 console.log(nombre); // imprime Wolverine
+
+// Resumen: let y const crean "variables" de scope. Var las crea de forma global, pero hay que evitar usarlas.
